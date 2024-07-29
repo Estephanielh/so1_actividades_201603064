e34acdc27dc3650a63da4e4be2d15ef360b10eb0
Estephanielh <3004689890101@ingenieria.usac.edu.gt>
2024-07-29 01:34:41 -0600

    script updates

--- actividad2/script.sh
+++ actividad2/script.sh
@@ -1,5 +1,10 @@
 GITHUB_USER="estephanielh"
 response=$(curl -s https://api.github.com/users/$GITHUB_USER)
-id=$(echo $response | jq '.id')
-created_at=$(echo $response | jq -r '.created_at')
-echo "Hola $GITHUB_USER. User ID: $id. Cuenta fue creada el: $created_at."                                                                                                                           
+id=$(echo $response | /home/linuxbrew/.linuxbrew/bin/jq '.id')
+created_at=$(echo $response | /home/linuxbrew/.linuxbrew/bin/jq -r '.created_at')
+today=$(date +'%Y-%m-%d')
+mkdir -p /tmp/$today && touch /tmp/$today/saludos.log
+echo "Hola $GITHUB_USER. User ID: $id. Cuenta fue creada el: $created_at." >> /tmp/$today/saludos.log
+
+# crontab -e
+# */5 * * * * /home/estephanielh/act2.sh >> /tmp/cron.log 2>&1
